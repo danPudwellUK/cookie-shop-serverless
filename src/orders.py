45b7723adec02e0d532bf078fab5a1d2527bba91
Dan Pudwell <dan.pudwell@gridsmartercities.com>
2019-09-17 11:40:20 +0100

    debugging

--- src/orders.py
+++ src/orders.py
@@ -11,14 +11,20 @@ ORDERS_DYNAMO_TABLE = os.environ.get("ORDERS_TABLE_NAME")
 def post_handler(event, context):
     try:
         dynamodb = boto3.resource("dynamodb").Table(ORDERS_DYNAMO_TABLE)
+        print("******************")
+        print(event)
         event_data = json.loads(event['body'])
+        print("******************")
+        print(event_data)
         data = {
             "id": str(uuid.uuid4()),
             "cookie_id": event_data['id'],
             "quantity": event_data['quantity']
         }
+        print("******************")
+        print(data)
         dynamodb.put_item(Item=data)
-
+        
         return {
             "statusCode": HTTPStatus.CREATED,
             "body": json.dumps(data)
