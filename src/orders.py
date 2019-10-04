45f416198016fdc6f88206631e2f320b78de0ce8
Dan Pudwell <dan.pudwell@gridsmartercities.com>
2019-10-04 13:02:43 +0100

    added swagger validation on post requests and 400 responses

--- src/orders.py
+++ src/orders.py
@@ -11,15 +11,12 @@ ORDERS_DYNAMO_TABLE = os.environ.get("ORDERS_TABLE_NAME")
 def post_handler(event, context):
     try:
         dynamodb = boto3.resource("dynamodb").Table(ORDERS_DYNAMO_TABLE)
-        
         event_data = json.loads(event['body'])
-        
         data = {
             "id": str(uuid.uuid4()),
             "cookie_id": event_data['cookie_id'],
             "quantity": event_data['quantity']
         }
-
         dynamodb.put_item(Item=data)
 
         return {
